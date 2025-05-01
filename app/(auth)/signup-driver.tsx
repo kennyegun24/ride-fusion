155755af03e47e5d8000f7415ce22d7b9a2e1525
Kennyegun24 <kennyegun240@gmail.com>
2025-05-01 02:34:11 +0100

    Signup for both roles done with firebase authentication

--- app/(auth)/signup-driver.tsx
+++ app/(auth)/signup-driver.tsx
@@ -11,8 +11,7 @@ import React, { FC, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { Header } from "@/components/auth/Header";
-import { Link, router } from "expo-router";
-import Select from "@/components/auth/Select";
+import { Link } from "expo-router";
 import AccountCreated from "@/components/auth/AccountCreated";
 import ValidateInput from "@/components/forms/ValidateInput";
 import { useForm } from "react-hook-form";
@@ -21,41 +20,89 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { driverSchema } from "@/schema/authValidationSchema";
 import { driverCredentials } from "@/utils/driverCredentials";
 import ValidateSelect from "@/components/forms/ValidateSelect";
-import { useAuthenticate } from "@/providers/AuthProvider";
-// import {OverScroll} from "@timotismjntk/react-native-overscroll-fix";
-// import { OverScrollView } from "react-native-overscroll";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db } from "@/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { FirebaseError } from "firebase/app";
+import axios from "axios";
+import { useRegistrationState } from "@/hooks/useRegisterationState";
+import { useRequest } from "@/providers/RequestProvider";
 type DriverFormValues = z.infer<typeof driverSchema>;
 
 const signupDriver = () => {
-  const [value, setValue] = useState<string>("");
   const [showModal, setShowModal] = useState(false);
-  const { authenticateUser } = useAuthenticate();
-  const register = () => {
-    setShowModal(true);
-    authenticateUser("driver");
-    setTimeout(() => {
-      router.push("/driver");
-    }, 2000);
+  const { triggerLoader, sending } = useRequest();
+  const register = async (e: any) => {
+    try {
+      triggerLoader(true);
+      const createUser = await createUserWithEmailAndPassword(
+        auth,
+        e?.email,
+        e?.password
+      );
+      const user = await createUser?.user;
+      // SAVE OTHER DETAILS TO DB
+      if (user) {
+        const dataToSend = {
+          uid: user.uid,
+          fullName: e.fullName,
+          email: e.email,
+          role: "driver",
+          phone: e.phone,
+          validPlatformAccounts: e.validPlatformAccounts,
+          driversLicenseType: e.driversLicenseType,
+          address: e.address,
+          yearsOfExperience: e.yearsOfExperience,
+        };
+        await updateProfile(user, {
+          displayName: e.fullName,
+        });
+        await setDoc(doc(db, "users", user.uid), {
+          ...dataToSend,
+          createdAt: new Date(),
+        });
+
+        const idToken = await user.getIdToken();
+
+        await axios.post(
+          "http://172.20.10.3:4000/api/auth/register",
+          { ...dataToSend },
+          {
+            headers: {
+              Authorization: `Bearer ${idToken}`,
+            },
+          }
+        );
+        useRegistrationState.getState().setRegistrationComplete(true);
+        return;
+      }
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      }
+    } finally {
+      setShowModal(true);
+      triggerLoader(false);
+    }
   };
   const { control, handleSubmit } = useForm<DriverFormValues>({
     resolver: zodResolver(driverSchema),
     defaultValues: {
-      fullName: "",
-      email: "",
-      phone: "",
-      password: "",
-      confirmPassword: "",
-      // driversLicense: "foreign",
-      basicDetails: "",
-      proofOfAddress: "",
-      faceCaptureVerification: "",
-      validPlatformAccounts: "",
-      yearsOfExperience: "",
+      fullName: "Kenny Egun",
+      email: "kennyegun240@gmail.com",
+      phone: "+2348025464789",
+      password: "Elias-2001",
+      confirmPassword: "Elias-2001",
+      address: "Block N18, flat 3, abesan estate, Ipaja, Lagos state",
+      // proofOfAddress: "",
+      // faceCaptureVerification: "",
+      // validPlatformAccounts: "",
+      yearsOfExperience: "2",
     },
   });
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {/* <StatusBar hidden={true} backgroundColor="red" /> */}
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.container}
@@ -66,17 +113,12 @@ const signupDriver = () => {
           subHeader="Book cars near you anytime, anywhere. Let's get you moving"
         />
         {showModal && <AccountCreated />}
-        {/* <OverScrollView
-          style={styles.container}
-          contentContainerStyle={styles.content}
-          alwaysBounceVertical
-        > */}
         <ScrollView
-          overScrollMode="always"
-          style={{ paddingVertical: 24 }}
+          // overScrollMode="always"
+          style={{ flex: 1 }}
           showsVerticalScrollIndicator={false}
         >
-          <View style={{ gap: 12 }}>
+          <View style={{ gap: 12, flex: 1, paddingVertical: 12 }}>
             {driverCredentials.map((e) => (
               <View key={e.key}>
                 {!e.options ? (
@@ -85,13 +127,19 @@ const signupDriver = () => {
                     name={e.key}
                     placeholder={e.placeholder}
                     control={control}
-                    secureTextEntry={e.key === "password"}
+                    secureTextEntry={e.key.toLowerCase().includes("password")}
+                    keyboardType={
+                      e.key === "email"
+                        ? "email-address"
+                        : e.key === "phone"
+                        ? "phone-pad"
+                        : "default"
+                    }
                   />
                 ) : (
                   <ValidateSelect
                     placeholder={e?.placeholder}
                     data={e.options}
-                    value={value}
                     control={control}
                     name={e.key}
                     label={e.label}
@@ -103,10 +151,18 @@ const signupDriver = () => {
         </ScrollView>
         {/* </OverScrollView> */}
         <View style={styles.btnContainer}>
-          <Pressable onPress={register} style={styles.authButton}>
+          <Pressable
+            onPress={sending ? null : handleSubmit(register)}
+            style={[
+              styles.authButton,
+              {
+                backgroundColor: sending ? "gray" : "#269355",
+              },
+            ]}
+          >
             <Text style={styles.authText}>Create Account</Text>
           </Pressable>
-          <Link href={"/(auth)/login"} style={styles.authButton2}>
+          <Link href={"/(auth)"} style={styles.authButton2}>
             <Text style={styles.authText2}>Already have an account</Text>
           </Link>
         </View>
@@ -124,22 +180,19 @@ const styles = StyleSheet.create({
     paddingTop: 12,
     position: "relative",
   },
-  content: {
-    paddingVertical: 20,
-  },
   scroll: { paddingVertical: 24 },
   labelStyle: { color: "#4B524E", fontWeight: 600, fontSize: 16 },
   authButton: {
     backgroundColor: "#269355",
     paddingHorizontal: 12,
-    paddingVertical: 16,
+    paddingVertical: 12,
     borderRadius: 50,
   },
-  btnContainer: { paddingVertical: 12 },
-  authText: { textAlign: "center", color: "#fff", fontSize: 18 },
+  btnContainer: { paddingVertical: 1, bottom: 0 },
+  authText: { textAlign: "center", color: "#fff", fontSize: 14 },
   authButton2: {
     paddingHorizontal: 12,
-    paddingVertical: 16,
+    paddingVertical: 12,
     borderRadius: 50,
   },
   authText2: { textAlign: "center", color: "#269355", fontSize: 14 },
--- app/(auth)/signup-owner.tsx
+++ app/(auth)/signup-owner.tsx
@@ -1,4 +1,6 @@
 import {
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   ScrollView,
   StyleSheet,
@@ -11,7 +13,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { Header } from "@/components/auth/Header";
 import { Link, router } from "expo-router";
-import Select from "@/components/auth/Select";
 import AccountCreated from "@/components/auth/AccountCreated";
 import { ownerCredentials } from "@/utils/ownerCredentials";
 import ValidateInput from "@/components/forms/ValidateInput";
@@ -20,89 +21,130 @@ import { useForm } from "react-hook-form";
 import { ownerSchema } from "@/schema/authValidationSchema";
 import { z } from "zod";
 import ValidateSelect from "@/components/forms/ValidateSelect";
-import { useAuthenticate } from "@/providers/AuthProvider";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db } from "@/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import axios from "axios";
+import { FirebaseError } from "firebase/app";
+import RequestSend from "@/components/global/requestSend";
+import { useRegistrationState } from "@/hooks/useRegisterationState";
 
 type OwnerFormValue = z.infer<typeof ownerSchema>;
 
 const signupDriver = () => {
-  const [value, setValue] = useState<string>("");
   const [showModal, setShowModal] = useState(false);
-  const { authenticateUser } = useAuthenticate();
-  const register = () => {
-    setShowModal(true);
-    authenticateUser("owner");
-    setTimeout(() => {
-      router.push("/(protected)/(tabs)");
-    }, 2000);
+  const [sending, setSending] = useState(false);
+  const register = async (e: any) => {
+    try {
+      setSending(true);
+      const createUser = await createUserWithEmailAndPassword(
+        auth,
+        e?.email,
+        e?.password
+      );
+      const user = await createUser?.user;
+      // SAVE OTHER DETAILS TO DB
+      if (user) {
+        const dataToSend = {
+          uid: user.uid,
+          fullName: e.fullName,
+          email: e.email,
+          role: "owner",
+          phone: e.phone,
+          address: e.address,
+        };
+        await setDoc(doc(db, "users", user.uid), {
+          ...dataToSend,
+          createdAt: new Date(),
+        });
+        await updateProfile(user, {
+          displayName: e.fullName,
+        });
+        const idToken = await user.getIdToken();
+        await axios.post(
+          "http://172.20.10.3:4000/api/auth/register",
+          { ...dataToSend },
+          {
+            headers: {
+              Authorization: `Bearer ${idToken}`,
+            },
+          }
+        );
+        useRegistrationState.getState().setRegistrationComplete(true);
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof FirebaseError) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      }
+    } finally {
+      setShowModal(true);
+      setSending(false);
+    }
   };
+
   const { control, handleSubmit } = useForm<OwnerFormValue>({
     resolver: zodResolver(ownerSchema),
     defaultValues: {
-      fullName: "",
-      mail: "",
-      phone: "",
-      password: "",
-      confirmPassword: "",
-      proofOfOwnership: "",
-      faceCaptureVerification: "",
-      carPictureWithPlate: "",
-      registrationDisk: "",
-      decra: "",
-      rentalValue: "",
-      rentalDeposit: "",
-      // isInsured: "",
-      // hasTracker: "",
-      // hasDashCam: "",
-      mileage: "",
-      engineSize: "",
+      fullName: "Elias Ken",
+      email: "kennyelias123@gmail.com",
+      phone: "+2348025464789",
+      password: "Elias2001-",
+      confirmPassword: "Elias2001-",
+      address: "Block N18",
     },
   });
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {/* <StatusBar hidden={true} backgroundColor="red" /> */}
-      {showModal && <AccountCreated />}
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Header
-          header="Create Your Account"
-          subHeader="Let other people rent your car for money!"
-        />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        {showModal && <AccountCreated />}
+        {sending && <RequestSend />}
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Header
+            header="Create Your Account"
+            subHeader="Let other people rent your car for money!"
+          />
 
-        <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
-          <View style={{ gap: 12 }}>
-            {ownerCredentials.map((e) => (
-              <View key={e.key}>
-                {e.type !== "boolean" ? (
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={{ gap: 12, flex: 1, paddingVertical: 12 }}>
+              {ownerCredentials.map((e) => (
+                <View key={e.key}>
                   <ValidateInput
                     label={e.label}
                     name={e.key}
                     placeholder={e.placeholder}
                     control={control}
-                    secureTextEntry={e.key === "password"}
+                    secureTextEntry={e.key.toLowerCase().includes("password")}
+                    keyboardType={
+                      e.key === "email"
+                        ? "email-address"
+                        : e.key === "phone"
+                        ? "phone-pad"
+                        : "default"
+                    }
                   />
-                ) : (
-                  <ValidateSelect
-                    placeholder={e?.placeholder}
-                    data={e.options}
-                    value={value}
-                    control={control}
-                    name={e.key}
-                    label={e.label}
-                  />
-                )}
-              </View>
-            ))}
+                </View>
+              ))}
+            </View>
+          </ScrollView>
+          <View style={styles.btnContainer}>
+            <Pressable
+              onPress={handleSubmit(register)}
+              style={styles.authButton}
+            >
+              <Text style={styles.authText}>Create Account</Text>
+            </Pressable>
+            <Link href={"/(auth)"} style={styles.authButton2}>
+              <Text style={styles.authText2}>Already have an account</Text>
+            </Link>
           </View>
-        </ScrollView>
-        <View style={styles.btnContainer}>
-          <Pressable onPress={register} style={styles.authButton}>
-            <Text style={styles.authText}>Create Account</Text>
-          </Pressable>
-          <Link href={"/(auth)/login"} style={styles.authButton2}>
-            <Text style={styles.authText2}>Already have an account</Text>
-          </Link>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -112,20 +154,10 @@ export default signupDriver;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 16,
+    paddingHorizontal: 8,
     paddingTop: 12,
   },
-  textInput: {
-    paddingHorizontal: 12,
-    paddingVertical: 16,
-    borderColor: "#E9E9E9",
-    borderWidth: 1,
-    borderRadius: 50,
-    marginTop: 6,
-    backgroundColor: "#171C2208",
-  },
-  scroll: { paddingVertical: 24 },
-  labelStyle: { color: "#4B524E", fontWeight: 600, fontSize: 18 },
+  // scroll: { paddingVertical: 24 },
   authButton: {
     backgroundColor: "#269355",
     paddingHorizontal: 12,
